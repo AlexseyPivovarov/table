a011a5457bcdd18abeac1c6f3caa75caf2a05c47
AlexseyPivovarov <a.peevovarov@gmail.com>
2019-03-29 15:29:51 +0000

    done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-table hover striped
+      :items="this.$store.state.crypto"
+      :fields="fields"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+    >
+      <template slot="CoinInfo.ImageUrl" slot-scope="data">
+        <img :src="'https://www.cryptocompare.com/' + data.item.CoinInfo.ImageUrl"
+             :alt="data.item.CoinInfo.ImageUrl"
+             height="30px">
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      timer: '',
+      sortBy: 'RAW.USD.PRICE',
+      sortDesc: true,
+      fields: [
+        { key: 'CoinInfo.ImageUrl', label: 'Image', sortable: false },
+        { key: 'CoinInfo.FullName', label: 'Full Name', sortable: true },
+        { key: 'CoinInfo.Name', label: 'Name', sortable: true },
+        { key: 'RAW.USD.PRICE', label: 'Price', sortable: true },
+        { key: 'RAW.USD.SUPPLY', label: 'Supply', sortable: true }
+      ]
+    }
+  },
+  methods: {
+    reload: function () {
+      this.$store.dispatch('getList')
+    }
+  },
+  beforeMount: function () {
+    this.reload()
+    this.timer = window.setInterval(this.reload, 300000)
+  },
+  beforeDestroy: function () {
+    window.clearInterval(this.timer)
   }
 }
 </script>
@@ -21,8 +52,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: start;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,37 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    crypto: [
+      {
+        CoinInfo: {
+          ImageUrl: '/media/19633/btc.png',
+          FullName: 'No Data',
+          Name: 'No Data'
+        },
+        RAW: {
+          USD: {
+            PRICE: 'No Data',
+            SUPPLY: 'No Data'
+          }
+        }
+      }
+    ]
   },
   mutations: {
-
+    setList (state, data) {
+      state.crypto = data
+      console.log('updated!')
+    }
   },
   actions: {
-
+    async getList ({ commit }) {
+      try {
+        let response = await Vue.http.get('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD', {})
+        commit('setList', response.body.Data)
+        return response.body.Data
+      } catch (e) {
+        console.log(e)
+      }
+    }
   }
 })
